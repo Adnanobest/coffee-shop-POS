7ca3d72367c16c306fa296380900dc555985ea32
Adnanobest <39516897+Adnanobest@users.noreply.github.com>
2024-12-06 22:04:50 +0300

    finished ManagementFrame

--- coffe shop POS/src/pack1/ManagmentFrame.java
+++ coffe shop POS/src/pack1/ManagmentFrame.java
@@ -14,49 +14,15 @@ import java.awt.Font;
 
 public class ManagmentFrame extends JFrame {
 
-	private boolean isNum(String str) {
-		int dot=0;
-		for (char x : str.toCharArray()) {
-			if(x=='.') {
-				if (++dot>1) {
-					JOptionPane.showMessageDialog(contentPane, "price can only be a number");
-					return false;
-				}
-			}else if (x-'0'<0 || x-'0'>9) {
-					JOptionPane.showMessageDialog(contentPane, "price can only be a number");
-					return false;
-			}
-		}
-		return true;
-	}
-
-
-	private void refresh(ArrayList<Drink> drinks, ArrayList<Dessert> desserts) {
-		listModel.clear();
-		listModel.addElement("Drinks:");
-		for (Drink x : drinks) {
-			listModel.addElement(x.name +", price: "+ x.getPrice());
-		}
-		listModel.addElement("Desserts:");
-		for (Dessert x : desserts) {
-			listModel.addElement(x.name +", price: "+ x.getPrice());
-		}
-	}
-
-
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtDrinkName;
 	private JTextField txtPrice;
 	private JTextField txtDessertName;
 	private JTextField txtDessertPrice;
-	private JLayeredPane addPane;
-	private JLayeredPane addDessertPane;	
-	private JLayeredPane addDrinkPane;
-	JButton btnAddDrink;
-	JButton btnAddDessert;
-	Font font = new Font("Tahoma", Font.PLAIN, 13);
 	DefaultListModel<String> listModel = new DefaultListModel<>();
+
+	Font font = new Font("Tahoma", Font.PLAIN, 13);
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -80,13 +46,17 @@ public class ManagmentFrame extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		addDrinkPane = new JLayeredPane();
+		JLayeredPane addPane = new JLayeredPane();
+		addPane.setBounds(0, 0,138,311);
+		contentPane.add(addPane);
+
+		JLayeredPane addDrinkPane = new JLayeredPane();
 		addDrinkPane.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		addDrinkPane.setBounds(138, 0, 207, 311);
 		contentPane.add(addDrinkPane);
 		addDrinkPane.setVisible(false);
 		
-		addDessertPane = new JLayeredPane();
+		JLayeredPane addDessertPane = new JLayeredPane();
 		addDessertPane.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		addDessertPane.setBounds(138, 0, 207, 311);
 		contentPane.add(addDessertPane);
@@ -96,25 +66,48 @@ public class ManagmentFrame extends JFrame {
 		listPane.setBounds(345, 0, 207, 311);
 		contentPane.add(listPane);
 		
-		JList<String> list = new JList<String>(listModel);
-		list.setBounds(0, 0, 207, 249);
-		listPane.add(list);
-		
-		JButton btnRemove = new JButton("Remove Selected Item");
-		btnRemove.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnRemove.setBounds(106, 269, 96, 42);
-		listPane.add(btnRemove);
-		
-		JButton btnChangePrice = new JButton("<html>Change <br>Price");
-		btnChangePrice.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnChangePrice.setBounds(5, 269, 96, 42);
-		listPane.add(btnChangePrice);
+		addPane(drinks, desserts, addDrinkPane, addDessertPane, addPane);
 
-		addPane = new JLayeredPane();
-		addPane.setBounds(0, 0,138,311);
-		contentPane.add(addPane);
-		
-		btnAddDrink = new JButton("add drink");
+		addDessertPane(desserts, drinks, addDessertPane);
+
+		addDrinkPane(drinks, desserts, addDrinkPane);
+
+		listPane(drinks, desserts, listPane);
+
+	
+	}
+	
+	private boolean isNum(String str) {
+		int dot=0;
+		for (char x : str.toCharArray()) {
+			if(x=='.') {
+				if (++dot>1) {
+					JOptionPane.showMessageDialog(contentPane, "price can only be a number");
+					return false;
+				}
+			}else if (x-'0'<0 || x-'0'>9) {
+					JOptionPane.showMessageDialog(contentPane, "price can only be a number");
+					return false;
+			}
+		}
+		return true;
+	}
+
+	private void refresh(ArrayList<Drink> drinks, ArrayList<Dessert> desserts) {
+		listModel.clear();
+		listModel.addElement("Drinks:");
+		for (Drink x : drinks) {
+			listModel.addElement(x.name +", price: "+ x.getPrice());
+		}
+		listModel.addElement("Desserts:");
+		for (Dessert x : desserts) {
+			listModel.addElement(x.name +", price: "+ x.getPrice());
+		}
+	}
+	
+	private void addPane(ArrayList<Drink> drinks, ArrayList<Dessert> desserts, JLayeredPane addDrinkPane,
+			JLayeredPane addDessertPane, JLayeredPane addPane) {
+		JButton btnAddDrink = new JButton("add drink");
 		btnAddDrink.setFont(font);
 		btnAddDrink.setBounds(5, 11,128, 42 );
 		addPane.add(btnAddDrink);
@@ -125,7 +118,7 @@ public class ManagmentFrame extends JFrame {
 			}
 		});
 		
-		btnAddDessert = new JButton("add dessert");
+		JButton btnAddDessert = new JButton("add dessert");
 		btnAddDessert.setFont(font);
 		btnAddDessert.setBounds(5, 65,128, 42 );
 		addPane.add(btnAddDessert);
@@ -145,51 +138,71 @@ public class ManagmentFrame extends JFrame {
 				addDessertPane.setVisible(true);
 			}
 		});
-
-	/*	ImageIcon icon = new ImageIcon(new ImageIcon("pics/coffee.jpg").getImage().getScaledInstance(118, 28, Image.SCALE_DEFAULT));
-		JLabel label= new JLabel(new ImageIcon(new ImageIcon("pics/coffee.jpg").getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
-		contentPane.add(label);
-		label.setBounds(100, 100, 150, 150); */
-
-
-		JLabel lblDessertName = new JLabel("Dessert name", JLabel.CENTER);
-		lblDessertName.setFont(font);
-		lblDessertName.setBounds(64, 22, 79, 16);
-		addDessertPane.add(lblDessertName);
-		
-		txtDessertName = new JTextField();
-		txtDessertName.setBounds(50, 50, 107, 20);
-		addDessertPane.add(txtDessertName);
-		txtDessertName.setColumns(10);
-		
-		JLabel lblDessertPrice = new JLabel("Price", JLabel.CENTER);
-		lblDessertPrice.setFont(font);
-		lblDessertPrice.setBounds(70, 82, 67, 14);
-		addDessertPane.add(lblDessertPrice);
-		
-		txtDessertPrice = new JTextField();
-		txtDessertPrice.setBounds(50, 108, 107, 20);
-		addDessertPane.add(txtDessertPrice);
-		txtDessertPrice.setColumns(10);
-				
-		JButton btnSaveDessert = new JButton("Save Dessert");
-		btnSaveDessert.setFont(font);
-		btnSaveDessert.setBounds(40, 258, 127, 42);
-		addDessertPane.add(btnSaveDessert);
-
-		btnSaveDessert.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(isNum(txtDessertPrice.getText())) {
-					String name=txtDessertName.getText();
-					Double price = Double.parseDouble(txtDessertPrice.getText());
-					desserts.add(new Dessert(name,price));
-					POS.ad(name);
-					txtDessertName.setText("");
-					txtDessertPrice.setText("");
+	}
+	
+	private void changePrice(ArrayList<Drink> drinks, ArrayList<Dessert> desserts, JList<String> list) {
+		if(!list.getSelectedValue().isEmpty()) {
+			String selected = list.getSelectedValue().split(",")[0];
+			String str = JOptionPane.showInputDialog(contentPane, "enter new price for "+ selected);
+			while (!isNum(str)) {
+				str = JOptionPane.showInputDialog(contentPane, "enter new price for "+ selected);
+			}
+			double newPrice =Double.parseDouble(str);
+			if(list.getSelectedIndex()>drinks.size()) {
+				for(Drink x : drinks) {
+					if(x.name.equals(selected)) {
+						x.setPrice(newPrice);
+						JOptionPane.showMessageDialog(contentPane,  selected+"'s price changed to"+ x.getPrice());
+						refresh(drinks, desserts);
+						return;
+					}
+				}
+			}else {
+				for(Dessert x : desserts) {
+					if(x.name.equals(selected)) {
+						x.setPrice(newPrice);
+						JOptionPane.showMessageDialog(contentPane,  selected+"'s price changed to"+ x.getPrice());
+						refresh(drinks, desserts);
+						return;
+					}	
 				}
 			}
+		}
+	}
+	
+	private void listPane(ArrayList<Drink> drinks, ArrayList<Dessert> desserts, JLayeredPane listPane) {
+		JList<String> list = new JList<String>(listModel);
+		list.setBounds(0, 0, 207, 249);
+		listPane.add(list);
+		
+		JLabel lblSelected = new JLabel("for selected item:");
+		lblSelected.setBounds(5, 255, 101, 16);
+		listPane.add(lblSelected);
+
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				remove(drinks, desserts, list);
+				refresh(drinks, desserts);
+			}
 		});
-				
+		btnRemove.setFont(font);
+		btnRemove.setBounds(106, 269, 96, 42);
+		listPane.add(btnRemove);
+		
+		JButton btnChangePrice = new JButton("<html>Change<br>Price");
+		btnChangePrice.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				changePrice(drinks, desserts, list);
+				refresh(drinks, desserts);
+			}
+		});
+		btnChangePrice.setFont(font);
+		btnChangePrice.setBounds(5, 269, 96, 42);
+		listPane.add(btnChangePrice);	
+	}
+
+	private void addDrinkPane(ArrayList<Drink> drinks, ArrayList<Dessert> desserts, JLayeredPane addDrinkPane) {
 		JLabel lblDrinkName = new JLabel("Drink name", JLabel.CENTER);
 		lblDrinkName.setFont(font);
 		lblDrinkName.setBounds(70, 22, 67, 16);
@@ -210,10 +223,10 @@ public class ManagmentFrame extends JFrame {
 		addDrinkPane.add(txtPrice);
 		txtPrice.setColumns(10);
 		
-		JLabel lblSize = new JLabel("Hot or cold", JLabel.CENTER);
-		lblSize.setFont(font);
-		lblSize.setBounds(65, 140, 77, 14);
-		addDrinkPane.add(lblSize);
+		JLabel lblHorC = new JLabel("Hot or cold", JLabel.CENTER);
+		lblHorC.setFont(font);
+		lblHorC.setBounds(65, 140, 77, 14);
+		addDrinkPane.add(lblHorC);
 		
 		JCheckBox chckbxHot = new JCheckBox("Hot");
 		chckbxHot.setFont(font);
@@ -246,7 +259,7 @@ public class ManagmentFrame extends JFrame {
 					drinks.add(new Drink(name,price,horc));
 					POS.ad(name);
 					JOptionPane.showMessageDialog(contentPane, drinks.getLast().name+" Saved with price "
-							+drinks.getLast().getPrice()+" with available sizes "+ horc);
+							+drinks.getLast().getPrice()+" with "+ horc +"available");
 					refresh(drinks, desserts);
 					txtDrinkName.setText("");
 					txtPrice.setText("");
@@ -257,9 +270,74 @@ public class ManagmentFrame extends JFrame {
 				}
 			}
 		});
-		
-	
 	}
+
+	private void addDessertPane(ArrayList<Dessert> desserts, ArrayList<Drink> drinks, JLayeredPane addDessertPane) {
+		JLabel lblDessertName = new JLabel("Dessert name", JLabel.CENTER);
+		lblDessertName.setFont(font);
+		lblDessertName.setBounds(64, 22, 79, 16);
+		addDessertPane.add(lblDessertName);
+		
+		txtDessertName = new JTextField();
+		txtDessertName.setBounds(50, 50, 107, 20);
+		addDessertPane.add(txtDessertName);
+		txtDessertName.setColumns(10);
+		
+		JLabel lblDessertPrice = new JLabel("Price", JLabel.CENTER);
+		lblDessertPrice.setFont(font);
+		lblDessertPrice.setBounds(70, 82, 67, 14);
+		addDessertPane.add(lblDessertPrice);
+		
+		txtDessertPrice = new JTextField();
+		txtDessertPrice.setBounds(50, 108, 107, 20);
+		addDessertPane.add(txtDessertPrice);
+		txtDessertPrice.setColumns(10);
+				
+		JButton btnSaveDessert = new JButton("Save Dessert");
+		btnSaveDessert.setFont(font);
+		btnSaveDessert.setBounds(40, 258, 127, 42);
+		addDessertPane.add(btnSaveDessert);
+
+		btnSaveDessert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(isNum(txtDessertPrice.getText())) {
+					String name=txtDessertName.getText();
+					Double price = Double.parseDouble(txtDessertPrice.getText());
+					desserts.add(new Dessert(name,price));
+					POS.ad(name);
+					JOptionPane.showMessageDialog(contentPane, desserts.getLast().name+" Saved with price "
+							+desserts.getLast().getPrice());
+					refresh(drinks, desserts);
+					txtDessertName.setText("");
+					txtDessertPrice.setText("");
+				}
+			}
+		});
+	}
+
+
+	private void remove(ArrayList<Drink> drinks, ArrayList<Dessert> desserts, JList<String> list) {
+		if (!list.getSelectedValue().isBlank()) {
+			if (list.getSelectedIndex()>drinks.size()) {
+				for (Drink x : drinks) {
+					if (x.name.equals(list.getSelectedValue())) {
+						JOptionPane.showMessageDialog(contentPane, list.getSelectedValue()+" deleted");
+						drinks.remove(x);
+						return;
+					}
+				}
+			}else {
+				for (Dessert x : desserts) {
+					if (x.name.equals(list.getSelectedValue())) {
+						JOptionPane.showMessageDialog(contentPane, list.getSelectedValue()+" deleted");
+						desserts.remove(x);
+						return;
+					}
+				}
+			}
+		}
+	}
+
 }
 
 		
