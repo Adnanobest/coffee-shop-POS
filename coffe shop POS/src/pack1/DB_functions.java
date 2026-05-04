8ad36ac357ebe02d7138c5fd99a1d61a647c8f9d
Nadeem24-02 <nadeem.naser2402@gmail.com>
2026-05-04 14:44:53 +0300

    addded JDBC

---
+++ coffe shop POS/src/pack1/DB_functions.java
@@ -0,0 +1,24 @@
+package pack1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_functions {
+
+	public static Connection getConnection() {
+        Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/coffee_shop_pos", "root", "Nn.2411@");
+		} catch (SQLException e) {
+			System.out.println();
+			e.printStackTrace();
+		}
+        return connection;
+    }
+	
+	Connection con = getConnection();
+	
+	 
+	
+}
